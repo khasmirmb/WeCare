3a3df0ade3a3cbf24461003f3525d4f8df59afa5
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-02 13:24:06 +0800

    Basic Laravel

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('auth/google', [GoogleAuthController::class, 'redirect'])->name('google-auth');
-Route::get('auth/google/call-back', [GoogleAuthController::class, 'callbackGoogle']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
