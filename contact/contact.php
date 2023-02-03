75aef48d9fa50f93a4dd4a7be1f4cc086a9a4b25
Kurt-Pobre <xt202001289@wmsu.edu.ph>
2023-02-03 23:27:04 +0800

    Contact Page fix- Bootstrap Use

--- contact/contact.php
+++ contact/contact.php
@@ -10,115 +10,111 @@
 ?>
 
 <!-- Contact Section Begin -->
-<div class="section-header">
-             <div class="contact_container">
-                 <h2 class="contact_h2">Contact Us</h2>
-                     <p class="contact_p">Getting in touch is now easy. Email us with any question or inquries. We would be happy to answer your questions</p>
+<!--Section: Contact v.2-->
+<section class="mb-4">
+
+    <!--Section heading-->
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+        a matter of hours to help you.</p>
+
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-9 mb-md-0 mb-5">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control">
+                            <label for="name" class="">First name</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control">
+                            <label for="name" class="">Last name</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="email" name="email" class="form-control">
+                            <label for="email" class="">Contact No.</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
                 </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Email</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            <label for="message">Your message</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+
+            <div class="text-center text-md-left">
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
             </div>
-
-<section class="contact spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                    <div class="contact__widget">
-                        <div class="contact__widget__icon">
-                            <i class="fa fa-facebook"></i>
-                        </div>
-                        <div class="contact__widget__text">
-                            <h5>Facebook</h5>
-                            <p>WeCare facebook</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                    <div class="contact__widget">
-                        <div class="contact__widget__icon">
-                            <i class="fa fa-twitter"></i>
-                        </div>
-                        <div class="contact__widget__text">
-                            <h5>twitter</h5>
-                            <p>@WeCare twitter</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                    <div class="contact__widget">
-                        <div class="contact__widget__icon">
-                            <i class="fa fa-google"></i>
-                        </div>
-                        <div class="contact__widget__text">
-                            <h5>Email</h5>
-                            <p>Support@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <section class="contact"> 
-                <div class="container_icon">
-                    <div class="contactInfo">
-                        <div class="box">
-                            <div class="icon"></div>
-                            <div class="text">
-                    <h3> Address <i class="fa fa-map-marker" aria-hidden="true"></i></h3>
-                    <p> Bernardo Drive, 143 S. De Leon Street, <br> Sta. Maria Road, Zamboanga, <br> 7000 Zamboanga del Sur </p>
-             </div>
-         </div>
-                <div class="box">
-                    <div class="icon"></div>
-                        <div class="text">
-                                <h3> Phone <i class="fa fa-phone" aria-hidden="true"></i> </h3>
-                                 <p> (062) 991 3236 </p>
-                            </div>
-                        </div>
-                <div class="box">
-                    <div class="icon"></div>
-                        <div class="text">
-                            <h3> Email <i class="fa fa-envelope-o" aria-hidden="true"></i></h3>
-                                <p> WC@yahoo.com </p>
-                            </div>
-                        </div>
-                    </div>
-
-                        <div class="contactForm">
-                            <form clas="cont_form">
-                                <h2> Send Message </h2>
-                                    <div class="inputBox">
-                                    <input type="text" name="" required="required">
-                                    <span> First Name </span>
-                                </div>  
-                                <div class="inputBox">
-                                    <input type="text" name="" required="required">
-                                    <span> Last Name </span>
-                                </div> 
-
-                                <div class="inputBox">
-                                    <input type="number" name="" required="required">
-                                    <span> Contact No. </span>
-                                </div>
-
-                                <div class="inputBox">
-                                    <input type="email" name="" required="required">
-                                    <span> Email </span>
-                                </div> 
-
-                                <div class="inputBox">
-                                    <textarea required="required"></textarea>
-                                    <span> Type your Message...</span>
-                                </div>  
-
-                            <div class="inputBox">
-                                <input type="submit" name="" value="send">
-                                </form>
-                        </div>
-                     </div>
-                </div>
-            </section>
+            <div class="status"></div>
         </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-3 text-center">
+            <ul class="list-unstyled mb-0">
+                <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <p>Bernardo Drive, 143 S. De Leon Street, Sta. Maria Road</p>
+                </li>
+
+                <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                    <p> (062) 991 3236</p>
+                </li>
+
+                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                    <p>contact@mdbootstrap.com</p>
+                </li>
+            </ul>
+        </div>
+        <!--Grid column-->
+
     </div>
-</div>
+
 </section>
+<!--Section: Contact v.2-->
     <!-- Contact Section End -->
 
 <?php
