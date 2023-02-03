a5fe4f702f99f79295043c08c20726db6fa81476
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-04 01:47:08 +0800

    New Design

--- includes/footer.php
+++ includes/footer.php
@@ -1,98 +1,64 @@
-<!-- Footer Section Begin -->
-<footer>
-        <div class="containers">
-            <div class="row">
-                  <div class="col" id="company">
-                      <img src="../img/logo.png" alt="" class="logo">
-                      <p>
-                        We specialized in taking care of your loved ones.
-                        Even away from home.
-                      </p>
-                      <div class="social">
-                        <a href="https://www.facebook.com/pages/We-Care-Nursing-Home-zamboanga-city/340004616038796"><i class="fab fa-facebook"></i></a>
-                        <a href="#"><i class="fab fa-google"></i></a>
-                      </div>
-                  </div>
+    <!-- Footer Start -->
+    <footer class="text-center text-white" style="background-color: #00ACB2;">
+    <!-- Grid container -->
+    <div class="container pt-4">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+        <!-- Facebook -->
+        <a
+            class="btn btn-link btn-floating btn-lg text-white m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-facebook-f"></i
+        ></a>
 
+        <!-- Twitter -->
+        <a
+            class="btn btn-link btn-floating btn-lg text-white m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="white"
+            ><i class="fab fa-twitter"></i
+        ></a>
 
-                  <div class="col" id="services">
-                     <h3>Services</h3>
-                     <div class="links">
-                        <a href="../services/services.php">Caregiving</a>
-                        <a href="#">Treatment</a>
-                        <a href="#">Consulting</a>
-                        <a href="#">Apply</a>
-                     </div>
-                  </div>
+        <!-- Google -->
+        <a
+            class="btn btn-link btn-floating btn-lg text-white m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="white"
+            ><i class="fab fa-google"></i
+        ></a>
 
-                  <div class="col" id="useful-links">
-                     <h3>Links</h3>
-                     <div class="links">
-                        <a href="../about/about.php">About</a>
-                        <a href="../faq/faq.php">FAQ</a>
-                        <a href="#">Our Policy</a>
-                        <a href="#">Help</a>
-                     </div>
-                  </div>
+        <!-- Instagram -->
+        <a
+            class="btn btn-link btn-floating btn-lg text-white m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="white"
+            ><i class="fab fa-instagram"></i
+        ></a>
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
 
-                  <div class="col" id="contact">
-                      <h3>Contact</h3>
-                      <div class="contact-details">
-                         <i class="fa fa-map-marker"></i>
-                         <p>Bernardo Drive, 143 S. De Leon Street, Sta. Maria Road, Zamboanga City</p>
-                      </div>
-                      <div class="contact-details">
-                         <i class="fa fa-phone"></i>
-                         <p>(062) 991 3236 </p>
-                      </div>
-                  </div>
-            </div>
-
-            <div class="row">
-                  <div class="form">
-                    <form action="">
-                        <input type="text" placeholder="Email here...">
-                        <button><i class="fa fa-paper-plane"></i></button>
-                    </form>
-                  </div>
-            </div>
-
-        </div>
-        <div class="footer__copyright">
-            <div class="cont">
-                <div class="row">
-                    <div class="col-lg-7">
-                      
-                        <div class="footer__copyright__text">
-                            <p>WeCare Nursing Home, Inc. Copyright</p>
-                        </div>
-                        
-                    </div>
-                    <div class="col-lg-5">
-                        <ul>
-                            <li>All Rights Reserved</li>
-                            <li>Terms & Use</li>
-                            <li>Privacy Policy</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <!-- Copyright -->
+    <div class="text-center text-dark p-3" style="background-color: white;">
+        © 2020 Copyright:
+        <a class="text-dark" href="#">WeCare.com</a>
+    </div>
+    <!-- Copyright -->
     </footer>
-    <!-- Footer Section End -->
 
-    <!-- Js Plugins -->
-    <script src="../js/jquery-3.3.1.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/jquery.magnific-popup.min.js"></script>
-    <script src="../js/masonry.pkgd.min.js"></script>
-    <script src="../js/jquery-ui.min.js"></script>
-    <script src="../js/jquery.nice-select.min.js"></script>
-    <script src="../js/jquery.slicknav.js"></script>
-    <script src="../js/owl.carousel.min.js"></script>
-    <script src="../js/main.js"></script>
-    <script src="../js/popup.js"></script>
+    <!-- Footer End -->
+
+    <!-- Font Awesome JS -->
+    <script src="https://kit.fontawesome.com/3a762edbf5.js" crossorigin="anonymous"></script>
+
+    <!--Bootstrap JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
-
 </html>
