0648768a0087ca0ed10548c75ddf3521ab22ac1e
Kurt-Pobre <xt202001289@wmsu.edu.ph>
2023-02-05 03:20:29 +0800

    Updates Home, Timeline

--- history/timeline.php
+++ history/timeline.php
@@ -8,61 +8,108 @@
 
 ?>
 
-<section class="gradient-custom-5">
-  <div class="container py-5">
-    <div class="main-timeline-5">
-      <div class="timeline-5 right-5">
-        <div class="card">
-          <div class="card-body p-4">
-            <h5>Title of section 1</h5>
-            <span class="small text-muted"><i class="fas fa-clock me-1"></i>21 March, 2020</span>
-            <p class="mt-2 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque
-              diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis
-              sagittis ligula in sodales vehicula....</p>
-          </div>
-        </div>
+<?php
+
+    $page_title = 'WeCare - Nursing Home, Inc.';
+    require_once '../includes/header.php';
+    session_start();
+
+    require_once '../includes/topnav.php';
+
+?>
+
+<h2 class="text-center py-3">WeCare Timeline</h2>
+
+<div class="container py-2 mt-4 mb-4">
+  <!-- timeline item 1 -->
+  <div class="row no-gutters">
+    <div class="col-sm"> <!--spacer--> </div>
+    <!-- timeline item 1 center dot -->
+    <div class="col-sm-1 text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col">&nbsp;</div>
+        <div class="col">&nbsp;</div>
       </div>
-      <div class="timeline-5 right-5">
-        <div class="card">
-          <div class="card-body p-4">
-            <h5>Title of section 2</h5>
-            <span class="small text-muted"><i class="fas fa-clock me-1"></i>5 April, 2020</span>
-            <p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam
-              non nisi semper.</p>
-            <p class="mb-0">Libero expedita explicabo eius fugiat quia aspernatur autem laudantium error
-              architecto recusandae natus sapiente sit nam eaque, consectetur porro molestiae ipsam! Deleniti.
-            </p>
-          </div>
-        </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
       </div>
-      <div class="timeline-5 right-5">
-        <div class="card">
-          <div class="card-body p-4">
-            <h5>Title of section 3</h5>
-            <span class="small text-muted"><i class="fas fa-clock me-1"></i>18 August, 2020</span>
-            <p class="mt-2 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque
-              diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis
-              sagittis ligula in sodales vehicula....</p>
-          </div>
-        </div>
-      </div>
-      <div class="timeline-5 right-5">
-        <div class="card">
-          <div class="card-body p-4">
-            <h5>Title of section 4</h5>
-            <span class="small text-muted"><i class="fas fa-clock me-1"></i>10 October, 2020</span>
-            <p class="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam
-              non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis
-              ligula in sodales vehicula....</p>
-            <p class="mb-0">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum
-              corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum
-              molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
-          </div>
+    </div>
+    <!-- timeline item 1 event content -->
+    <div class="col-sm py-2">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">2001</h4>
+          <p class="card-text">On October of 2001, three cousins founded a non-profit organization named WeCare Nursing Home INC by Jarah Bernardo, Sheila, Padua and Gianelli Delos Santos on Governor Camins Avenue, Zamboanga City but later on transfered to barangay Boalan near Inland Resort Hotel & Restaurant.</p>
         </div>
       </div>
     </div>
   </div>
-</section>
+  <!--/row-->
+  <!-- timeline item 2 -->
+  <div class="row no-gutters">
+    <div class="col-sm py-2">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">2001 - 2012</h4>
+          <p class="card-text">Years later, they transfered to Sta. Maria during the year of 2012 </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-1 text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col-sm"> <!--spacer--> </div>
+  </div>
+  <!--/row-->
+  <!-- timeline item 3 -->
+  <div class="row no-gutters">
+    <div class="col-sm"> <!--spacer--> </div>
+    <div class="col-sm-1 text-center flex-column d-none d-sm-flex">
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+      <h5 class="m-2">
+        <span class="badge badge-pill bg-light border">&nbsp;</span>
+      </h5>
+      <div class="row h-50">
+        <div class="col border-right">&nbsp;</div>
+        <div class="col">&nbsp;</div>
+      </div>
+    </div>
+    <div class="col-sm py-2">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">2012 - 2022 </h4>
+          <p>They are already 21 years in service and nurses aged people up until today.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/row-->
+</div>
+
+
+
+<?php
+
+    require_once '../includes/footer.php';
+
+?>
 
 <?php
 
