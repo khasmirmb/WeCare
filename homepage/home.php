8e7ef0edb00fa0222c345b65d9715d90b3e4dab9
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-05 01:08:55 +0800

    Update home.php

--- homepage/home.php
+++ homepage/home.php
@@ -1,15 +1,15 @@
 <?php
-    $page_title = 'WeCare - Nursing Home, Inc.';
+
+    $page_title = 'WeCare - Home';
     require_once '../includes/header.php';
+    require_once '../classes/account.class.php';
     session_start();
 
-    require_once '../includes/topnav.php';
-
+    require_once '../includes/navbar.php';
 ?>
 
-<main>
-<!-- Carousel Start -->
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <!-- Carousel Start -->
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -18,34 +18,34 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img src="../images/home-bg.jpg" class="d-block w-100" alt="...">
+                <img src="../images/home-display1.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Why Choose WeCare?</h5>
-                    <p class="caroul-p">WeCare Offers A lot of Perks That Our Costumer Needs</p>
-                    <p><a href="../about/about.php" class="btn btn-warning mt3">Learn More</a></p>
+                    <p>WeCare Offers A lot of Perks That Our Costumer Needs.</p>
+                    <p><a href="#" class="btn btn-warning mt3">Learn More</a></p>
                 </div>
                 </div>
                 <div class="carousel-item">
-                <img src="../images/service1.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
+                <img src="../images/home-display2.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Find Best Care Service With WeCare</h5>
-                    <p class="caroul-p">WeCare Offers Years of Experience in Caring</p>
-                    <p><a href="../services/services.php" class="btn btn-warning mt3">Learn More</a></p>
+                    <p>WeCare Offers Years of Experience in Caring.</p>
+                    <p><a href="#" class="btn btn-warning mt3">Learn More</a></p>
                 </div>
                 </div>
                 <div class="carousel-item">
-                <img src="../images/display-5.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
+                <img src="../images/home-display3.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Our Own Area</h5>
-                    <p class="caroul-p">Walking Area, Terrace and Dinning Area</p>
-                    <p><a href="../rooms/rooms.php" class="btn btn-warning mt3">Learn More</a></p>
+                    <p>Walking Area, Terrace and Dinning Area</p>
+                    <p><a href="#" class="btn btn-warning mt3">Learn More</a></p>
                 </div>
                 </div>
                 <div class="carousel-item">
-                <img src="../images/display-4.jpg" class="d-block w-100" alt="..." style="filter: brightness(50%);">
+                <img src="../images/home-display4.jpg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Our Services</h5>
-                    <p class="caroul-p">Caregiving, Memory Care, Rehabilitation, Long Term Care</p>
+                    <p>Caregiving, Memory Care, Rehabilitation, Long Term Care</p>
                     <p><a href="#" class="btn btn-warning mt3">Learn More</a></p>
                 </div>
                 </div>
@@ -60,11 +60,9 @@
             </button>
         </div>
         <!-- Carousel End -->
-<main>
-
 
 <?php
 
-    require_once '../includes/footer.php';
+require_once '../includes/footer.php';
 
 ?>
