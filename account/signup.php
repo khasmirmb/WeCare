9d66af3aa7f94542ab36ca16cf661340106fd489
Kurt-Pobre <xt202001289@wmsu.edu.ph>
2023-02-06 21:15:39 +0800

    New updates

--- account/signup.php
+++ account/signup.php
@@ -5,67 +5,64 @@
     session_start();
 ?>
 
-<div class="py-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>SignUp Form</h4>
-                    </div>
-                    <div class="card-body">
-                        <form action="" class="need-validation">
-                            <div class="form-group mb-3">
-                                <label for="">FirstName</label>
-                                <input type="text" name="firstname"class="form-control" required>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="">LastName</label>
-                                <input type="text" name="lastname"class="form-control" required>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="">Email Address</label>
-                                <input type="text" name="email"class="form-control" required>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="">Password</label>
-                                <input type="password" name="password"class="form-control" required>
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="">Confirm Password</label>
-                                <input type="password" name="confirm_password"class="form-control" required>
-                            </div>
-                            <div class="form-group text-center">
-                                <button type="Submit" class="btn btn-primary login-button">Create Account</button>
-                            </div>
-                            <div class="col-md">
-                                <hr class="mt-4 mb-4">
-                            </div>
-                            <div class="form-group mt-3">
-                                <p class="text-center continue-google">
-                                <a href="#" class="btn btn-primary custom">
-                                <i class="fa-brands fa-google"></i>
-                                SignUp with Google
-                                </a>
-                                </p>
-                            </div>
-                            <div class="form-group mt-1">
-                                <p class="text-center continue-google">
-                                <a href="#" class="btn btn-primary custom">
-                                <i class="fa-brands fa-facebook"></i>
-                                SignUp with Facebook
-                                </a>
-                                </p>
-                            </div>
-                        </form>
-                        <div class="row mt-3">
-                            <p class="text-center">
-                                Already have an account? <a class="green" href="signin.php">Sign in</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-xl-9 mx-auto">
+        <div class="card flex-row my-5 border-0 shadow rounded-3 overflow-hidden">
+          <div class="card-img-left d-none d-md-flex">
+            <!-- Background image for card set in CSS! -->
+          </div>
+          <div class="card-body p-4 p-sm-5">
+            <h5 class="card-title text-center mb-5 fw-light fs-5">Sign Up</h5>
+            <form>
+
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInputUsername" placeholder="myusername" required autofocus>
+                <label for="floatingInputUsername">Username</label>
+              </div>
+
+              <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInputEmail" placeholder="name@example.com">
+                <label for="floatingInputEmail">Email address</label>
+              </div>
+
+              <hr>
+
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+              </div>
+
+              <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPasswordConfirm" placeholder="Confirm Password">
+                <label for="floatingPasswordConfirm">Confirm Password</label>
+              </div>
+
+              <div class="d-grid mb-2">
+                <button class="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit">Register</button>
+              </div>
+
+              <a class="d-block text-center mt-2 small" href="../account/signin.php">Have an account? Sign In</a>
+
+              <hr class="my-4">
+
+              <div class="d-grid mb-2">
+                <button class="btn btn-lg btn-google btn-login fw-bold text-uppercase" type="submit">
+                  <i class="fab fa-google me-2"></i> Sign up with Google
+                </button>
+              </div>
+
+              <div class="d-grid">
+                <button class="btn btn-lg btn-facebook btn-login fw-bold text-uppercase" type="submit">
+                  <i class="fab fa-facebook-f me-2"></i> Sign up with Facebook
+                </button>
+              </div>
+
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
+</body>
