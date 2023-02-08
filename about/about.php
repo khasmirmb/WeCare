ab78545f7130f24d161b97a47836b936f3327f2c
Kurt-Pobre <xt202001289@wmsu.edu.ph>
2023-02-08 21:24:42 +0800

    About and additional Content Updates

--- about/about.php
+++ about/about.php
@@ -9,12 +9,13 @@
 ?>
 
          <!-- About Section Begin -->
-   <section class="about spad">
+         <section class="about spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-md-6">
-                    <div class="about__us set-bg" data-setbg="../images/home-display4.jpg">
-                    </div>
+                <div class="image">
+                    <img src="../images/home-display1.jpg" class="rounded mx-auto d-block" alt="">
+                </div>
                 </div>
                 <div class="col-lg-6 col-md-6">
                     <div class="about__text">
