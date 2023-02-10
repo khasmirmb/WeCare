9b9e89fd2290968b7c2fb16ca2cd432b6a0fb5ad
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2023-02-10 23:23:07 +0800

    database
    para sa website natin na database na maayos na

---
+++ classes/account.class.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'database.php';
+
+class Account{
+
+    public $id;
+    public $email;
+    public $password;
+    public $type;
+
+    protected $db;
+
+    function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    function sign_in(){
+        $sql = "SELECT * FROM account WHERE BINARY email = :email AND BINARY password = :password;";
+        $query=$this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+        $query->bindParam(':password', $this->password);
+        if($query->execute()){
+            if($query->rowCount()>0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function get_account_info($id=0){
+        if($id == 0){
+            $sql = "SELECT * FROM account WHERE BINARY email = :email AND BINARY password = :password;";
+            $query=$this->db->connect()->prepare($sql);
+            $query->bindParam(':email', $this->email);
+            $query->bindParam(':password', $this->password);
+        }else{
+            $sql = "SELECT * FROM account WHERE id = :id;";
+            $query=$this->db->connect()->prepare($sql);
+            $query->bindParam(':id', $id);
+        }
+        if($query->execute()){
+            $data = $query->fetchAll();
+        }
+        return $data;
+    }
+
+}
+
+?>
