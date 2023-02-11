ce3d3a41a362ee56e6c96fd397a9fae53a5b1192
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-12 07:02:21 +0800

    Updates

--- contact/contact.php
+++ contact/contact.php
@@ -14,7 +14,8 @@
                     <!--Section heading-->
                     <h2 class="h1-responsive font-weight-bold text-center my-4">Contact Us</h2>
                     <!--Section description-->
-                    <p class="text-center w-responsive mx-auto mb-4"></p>
+                    <p class="text-center w-responsive mx-auto mb-4">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+                    a matter of hours to help you.</p>
                 </div>
             </div>
 
