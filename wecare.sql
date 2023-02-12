22f10d9616239487b8936809d2e8287eb767a2a7
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-12 21:52:38 +0800

    Updated Tables not Finalized

--- wecare.sql
+++ wecare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2023 at 11:43 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Feb 12, 2023 at 02:51 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,65 @@ SET time_zone = "+00:00";
 --
 -- Database: `wecare`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `appointment_num` int(11) NOT NULL,
+  `purpose` int(11) NOT NULL,
+  `others` text NOT NULL,
+  `staff_id` int(11) NOT NULL,
+  `appointment_date` date NOT NULL,
+  `appointment_time` time NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment_purpose`
+--
+
+CREATE TABLE `appointment_purpose` (
+  `id` int(11) NOT NULL,
+  `purpose` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `is_relative` int(11) NOT NULL,
+  `relative_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monitoring_request`
+--
+
+CREATE TABLE `monitoring_request` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `files` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -42,16 +101,18 @@ CREATE TABLE `patient` (
   `postal` int(50) NOT NULL,
   `background_history` longtext NOT NULL,
   `doctors_diagnosis` longtext NOT NULL,
-  `allergies` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `allergies` longtext NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `patient`
 --
 
-INSERT INTO `patient` (`id`, `firstname`, `lastname`, `suffix`, `date_of_birth`, `place_of_birth`, `gender`, `province`, `street`, `barangay`, `city`, `postal`, `background_history`, `doctors_diagnosis`, `allergies`) VALUES
-(1, 'Datu', 'Batumbakal', 'Jr', '1994-02-11', 'Manila', 'Male', 'Manila', 'Manila', 'Manila', 'Manila', 7000, '12412412412', '1241241241241', '12412412412'),
-(2, 'fasdfasdf', 'sadfasdfsad', 'asdfsd', '2012-04-13', 'sdfsdfsd', 'sdfsdf', 'sdfsdf', 'sdfsd', 'fsdfsdfsd', 'sdfsdf', 8080, 'sdfsdf', 'sdfsdf', 'sdfsdfsd');
+INSERT INTO `patient` (`id`, `firstname`, `lastname`, `suffix`, `date_of_birth`, `place_of_birth`, `gender`, `province`, `street`, `barangay`, `city`, `postal`, `background_history`, `doctors_diagnosis`, `allergies`, `picture`, `status`) VALUES
+(1, 'Datu', 'Batumbakal', 'Jr', '1994-02-11', 'Manila', 'Male', 'Manila', 'Manila', 'Manila', 'Manila', 7000, '12412412412', '1241241241241', '12412412412', '', 0),
+(2, 'fasdfasdf', 'sadfasdfsad', 'asdfsd', '2012-04-13', 'sdfsdfsd', 'sdfsdf', 'sdfsdf', 'sdfsd', 'fsdfsdfsd', 'sdfsdf', 8080, 'sdfsdf', 'sdfsdf', 'sdfsdfsd', '', 0);
 
 -- --------------------------------------------------------
 
@@ -64,8 +125,128 @@ CREATE TABLE `patient_monitoring` (
   `patient_id` int(11) NOT NULL,
   `room` varchar(50) NOT NULL,
   `status` varchar(50) NOT NULL,
-  `nurse_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `staff_id` int(11) NOT NULL,
+  `relative_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `month` varchar(100) NOT NULL,
+  `amount` int(30) NOT NULL,
+  `date` date NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_status`
+--
+
+CREATE TABLE `payment_status` (
+  `id` int(11) NOT NULL,
+  `status` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `relationship`
+--
+
+CREATE TABLE `relationship` (
+  `id` int(11) NOT NULL,
+  `relationship` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `relationship`
+--
+
+INSERT INTO `relationship` (`id`, `relationship`) VALUES
+(1, 'Mother'),
+(2, 'Father'),
+(3, 'Parents'),
+(4, 'Grandmother'),
+(5, 'Grandfather'),
+(6, 'Grandparents'),
+(7, 'Mother In Law'),
+(8, 'Father In Law'),
+(9, 'Aunt'),
+(10, 'Uncle'),
+(11, 'Other Relative'),
+(12, 'Friend');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `relative`
+--
+
+CREATE TABLE `relative` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `middlename` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `suffix` varchar(50) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `place_of_birth` varchar(100) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `street` varchar(100) NOT NULL,
+  `barangay` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `postal` varchar(100) NOT NULL,
+  `relationship` int(11) NOT NULL,
+  `phone_number` int(11) NOT NULL,
+  `telephone_number` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `joined_date` date NOT NULL,
+  `retired_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id`, `status`) VALUES
+(1, 'Accepted'),
+(2, 'Rejected'),
+(3, 'Pending'),
+(4, 'Completed');
 
 -- --------------------------------------------------------
 
@@ -75,19 +256,19 @@ CREATE TABLE `patient_monitoring` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `unique_id` int(50) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `phone` varchar(25) NOT NULL,
+  `image` varchar(255) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `type` int(11) NOT NULL DEFAULT 1,
+  `otp` int(50) NOT NULL,
+  `verification_status` varchar(50) NOT NULL,
+  `type` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password`, `type`, `created_at`, `updated_at`) VALUES
-(1, 'user@gmail.com', 'user', 1, '2023-02-11 20:00:18', '2023-02-11 21:16:54');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -98,7 +279,7 @@ INSERT INTO `users` (`id`, `email`, `password`, `type`, `created_at`, `updated_a
 CREATE TABLE `user_type` (
   `id` int(11) NOT NULL,
   `type` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_type`
@@ -115,6 +296,31 @@ INSERT INTO `user_type` (`id`, `type`) VALUES
 --
 
 --
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `appointment_purpose`
+--
+ALTER TABLE `appointment_purpose`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_client` (`user_id`);
+
+--
+-- Indexes for table `monitoring_request`
+--
+ALTER TABLE `monitoring_request`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `patient`
 --
 ALTER TABLE `patient`
@@ -124,6 +330,42 @@ ALTER TABLE `patient`
 -- Indexes for table `patient_monitoring`
 --
 ALTER TABLE `patient_monitoring`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `relationship`
+--
+ALTER TABLE `relationship`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `relative`
+--
+ALTER TABLE `relative`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -143,6 +385,30 @@ ALTER TABLE `user_type`
 --
 
 --
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `appointment_purpose`
+--
+ALTER TABLE `appointment_purpose`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `monitoring_request`
+--
+ALTER TABLE `monitoring_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
@@ -155,16 +421,62 @@ ALTER TABLE `patient_monitoring`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment_status`
+--
+ALTER TABLE `payment_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `relationship`
+--
+ALTER TABLE `relationship`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `relative`
+--
+ALTER TABLE `relative`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_type`
 --
 ALTER TABLE `user_type`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `client`
+--
+ALTER TABLE `client`
+  ADD CONSTRAINT `user_client` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
