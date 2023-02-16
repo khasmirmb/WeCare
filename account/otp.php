94b8afa9d504679f0fc4d916217875e9f7eebbfb
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-16 23:18:04 +0800

    Test

--- account/otp.php
+++ account/otp.php
@@ -3,6 +3,7 @@
 session_start();
 
 include_once '../classes/basic.database.php';
+require_once '../classes/users.class.php';
 
 $otp1 = $_POST['otp1'];
 $otp2 = $_POST['otp2'];
@@ -11,6 +12,8 @@ $otp4 = $_POST['otp4'];
 $unique_id = $_SESSION['unique_id'];
 $session_otp = $_SESSION['otp'];
 $otp = $otp1.$otp2.$otp3.$otp4;
+
+$users_account = new Users();
 
 if(!empty($otp)){
     if($otp == $session_otp){
@@ -23,6 +26,25 @@ if(!empty($otp)){
                 $_SESSION['unique_id'] = $row['unique_id'];
                 $_SESSION['verification_status'] = $row['verification_status'];
                 echo "Success";
+                if($users_account->sign_in()){
+                    $users = $users_account->get_user_info();
+                    foreach($users as $row){
+                        $_SESSION['logged_id'] = $row['id'];
+                        $_SESSION['fullname'] = $row['fname'] . ' ' . $row['lname'];
+                        $_SESSION['user_type'] = $row['type'];
+                        $_SESSION['unique_id'] = $row['unique_id'];
+                        $_SESSION['profile_pic'] = $row['image'];
+                        $_SESSION['verification_status'] = $row['verification_status'];
+                        //display the appropriate dashboard page for user
+                        if($row['type'] == 'admin'){
+                          header('location: ../admin/dashboard.php');
+                        }else if($row['type'] == 'staff'){
+                          header('location: ../staff/dashboard.php');
+                        }else if($row['type'] == 'client'){
+                         header('location: ../homepage/home.php');
+                        }
+                      }
+                    }
 
             }
 
