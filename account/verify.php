87c030dd58363ab5d03902f962c1ae0a1f1bf478
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-16 22:35:48 +0800

    Updates

--- account/verify.php
+++ account/verify.php
@@ -7,6 +7,21 @@
     
     session_start();
 
+    $unique_id = $_SESSION['unique_id'];
+    if(empty($unique_id)){
+      header("Location: signin.php");
+    }
+    $qry = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = '{$unique_id}'");
+    if(mysqli_num_rows($qry) > 0){
+      $row = mysqli_fetch_assoc($qry);
+      if($row){
+        $_SESSION['verification_status'] = $row['verification_status'];
+        if($row['verification_status'] != 'Verified'){
+           header("location: verify.php");
+        }
+      }
+    }
+
 ?>
     <nav class="navbar bg-#fff">
       <div class="container-fluid ms-2">
