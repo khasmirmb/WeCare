4e7413b72f60a45b91626cdfe50ff3cb6cc9bd94
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-16 22:16:56 +0800

    Updates

--- account/signin.php
+++ account/signin.php
@@ -20,7 +20,7 @@
         $users = $users_account->get_user_info();
         foreach($users as $row){
             $_SESSION['logged_id'] = $row['id'];
-            $_SESSION['fullname'] = $row['firstname'] . ' ' . $row['lastname'];
+            $_SESSION['fullname'] = $row['fname'] . ' ' . $row['lname'];
             $_SESSION['user_type'] = $row['type'];
             $_SESSION['unique_id'] = $row['unique_id'];
             $_SESSION['profile_pic'] = $row['image'];
