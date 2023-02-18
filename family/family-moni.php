4fdc95fafb987a29baf831b31d99d86d0cd0c2ac
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-02-18 22:21:17 +0800

    Monitoring System
    Hindi pa tapos may changes pa siya naga loko loko ang container and back button

---
+++ family/family-moni.php
@@ -0,0 +1,43 @@
+<?php
+
+    $page_title = 'WeCare - Family Monitoring';
+    require_once '../includes/header.php';
+    require_once '../classes/account.class.php';
+    session_start();
+
+    require_once '../includes/navbar.php';
+?>
+
+<div class="p-5">
+    <h2 class="pb-3"><strong>Family Monitoring</strong></h2>
+    <table class="table table-hover table-striped table-bordered">
+    <thead class="table-info ">
+        <tr>
+        <th scope="col">Patient Name</th>
+        <th cope="col" class="text-center">Relationship</th>
+        <th scope="col" class="text-center">Room</th>
+        <th scope="col" class="text-center">Status</th>
+        <th scope="col" class="text-center">Nurse</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td>Al-khasmir Basaluddin</td>
+        <td class="text-center">Father</td>
+        <td class="text-center">Room 1</td>
+        <td class="text-center">Active</td>
+        <td class="text-center">Mikaylah Chu</td>
+        </tr>
+        <tr>
+        <td>Rob Roche Villanueva</td>
+        <td class="text-center">Uncle</td>
+        <td class="text-center">Room 3</td>
+        <td class="text-center">Active</td>
+        <td class="text-center">Mikaylah Chu</td>
+        </tr>
+    </tbody>
+</table>
+<div class="d-grid gap-2 d-md-flex justify-content-md-end">
+    <button type="button" class="btn btn-primary"><a class="text-white text-decoration-none" href="monitor-request.php">Request Monitoring</a></button>
+    </div>
+</div>
