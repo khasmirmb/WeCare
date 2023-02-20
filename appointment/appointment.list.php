e7c2749c9044cfd90289c390f5d4437e7cfa160c
khasmirmb <xt202001178@wmsu.edu.ph>
2023-02-21 01:22:19 +0800

    Unfinished Appointment List

---
+++ appointment/appointment.list.php
@@ -0,0 +1,59 @@
+<?php
+
+    $page_title = 'WeCare - Appointment';
+    require_once '../includes/header.php';
+    session_start();
+
+    if(!isset($_SESSION['logged_id'])){
+        header('location: ../account/signin.php');
+    }
+
+    require_once '../includes/navbar.php';
+
+    require_once '../classes/appointment.class.php';
+
+    $appointments = new Appointment();
+
+    $appointment_list = $appointments->show_appointment_data();
+    
+
+?>
+    <div class="table-responsive">
+        <table class="table caption-top">
+        <caption>List of Appointments</caption>
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+
+<?php
+
+require_once '../includes/footer.php';
+
+?>
