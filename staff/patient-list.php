3a0a702e26fc8d5987a58accd54d4804c5f9133f
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-02-21 15:41:24 +0800

    Not Done Yet

---
+++ staff/patient-list.php
@@ -0,0 +1,14 @@
+<?php
+
+    $page_title = 'Staff - Patient List';
+    require_once '../includes/header.php';
+    require_once '../classes/account.class.php';
+    session_start();
+
+    require_once '../includes/navbar.php';
+?>
+<div class="container align-items-center pt-3">
+<div class="bg-primary p-5">
+    <img src="\images\amongus2.jpg" class="img-thumbnail" alt="among us">
+</div>
+</div>
