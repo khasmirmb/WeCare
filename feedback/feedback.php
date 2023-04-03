71a0f5eacd5cfa95af465ceed182add3c7734389
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-04-03 22:05:00 +0800

    Done feedback
    I want to put the modal/ alert in the send button

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -13,3 +13,30 @@
     
 
 ?>
+<div class="content pt-3">
+
+    <div class="container">
+    <div class="card">
+    <h4 class="card-header">Give Feedback</h4> <!--header-->
+  <div class="card-body">
+    <div class="form-group">
+      <label for="rate-experience" class="form-label">How would you rate your experience?</label>
+      <div class="text-center mb-3"><!--Emoticons start--->
+         <button class="border rounded-pill"><i class="fa-regular fa-face-smile-beam fa-4x"></i></button> <!--icon-->
+        <button class="border rounded-pill"><i class="fa-regular fa-face-smile fa-4x"></i></i></button><!--icon-->
+        <button class="border rounded-pill"><i class="fa-regular fa-face-meh fa-4x"></i></button><!--icon-->
+        <button class="border rounded-pill"><i class="fa-regular fa-face-frown fa-4x"></i></button><!--icon-->
+        <button class="border rounded-pill"><i class="fa-regular fa-face-angry fa-4x"></i></button><!--icon-->
+      </div><!--Emoticons end-->
+      <label for="experience" class="form-label">Please tell us about your experience:</label>
+      <textarea class="form-control" id="experience" rows="3"></textarea>
+      <br>
+      <label for="email-input" class="form-label">Your email address (optional):</label>
+      <input type="email" class="form-control" id="email-input">
+      <br>
+      <a href="#" class="btn btn-primary">Send</a><!--button to send-->
+    </div>
+  </div>
+</div>
+</div>
+</div><!--end of the content-->
