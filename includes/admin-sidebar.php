bfc8dcec18b02770381f886f9c7dcbd61988cf76
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-04-23 21:52:31 +0800

    some changes

--- includes/admin-sidebar.php
+++ includes/admin-sidebar.php
@@ -45,21 +45,21 @@
       <a href="../admin/admission.php"><i class="fas fa-clipboard-list"></i><span>Admission</span></a>
       <a href="../admin/appointment.php"><i class="fas fa-calendar"></i><span>Appointment</span></a>
       <a href="../admin/setting.php"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
-      <a href="../account/logout.php" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-right-from-bracket"></i><span>Logout</span></a>
+      <a href="../account/logout.php" data-bs-toggle="modal" data-bs-target="#logout"><i class="fas fa-right-from-bracket"></i><span>Logout</span></a>
     </div>
     <!--sidebar end-->
 
-    <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
+     <!-- Modal -->
+<div class="modal fade" id="logout" tabindex="-1" aria-labelledby="logoutLabel" aria-hidden="true" data-bs-backdrop="static" aria-labelledby="logoutLabel">
+  <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Do you want to logout?</h5>
+        <h5 class="modal-title" id="logoutLabel">Do you want to logout?</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-footer">
-        <button type="button" class="close-btn" data-bs-dismiss="modal">Close</button>
-        <a href="../account/logout.php"><button type="button" class="yes-btn">Yes</button></a>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a href="../account/logout.php"><button type="button" class="btn btn-primary">Yes</button></a>
       </div>
     </div>
   </div>
