7a37e9c5523fc34610b98b7ba2e8133ee268eb3a
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-04-26 22:42:33 +0800

    small

--- family/patient.monitoring.php
+++ family/patient.monitoring.php
@@ -322,9 +322,6 @@
         <li class="list-group-item">Aila Chrishel Dagalea</li>
         </ul>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
     </div>
   </div>
 </div>
