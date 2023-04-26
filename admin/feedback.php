a37f19e49b314eb7b50ef59d87f2a4c614b14ae8
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-04-27 00:31:57 +0800

    small

--- admin/feedback.php
+++ admin/feedback.php
@@ -36,7 +36,7 @@
         </ul>
 	</div>
 
-    <div class="tab-content" id="pills-tabContent">
+    <div class="tab-content pt-3" id="pills-tabContent">
         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
 
         <!-- Feedback Here -->
