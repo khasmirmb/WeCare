4803d53288df77bfbb387eccc07e1b4d19fe7334
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2023-05-05 20:33:06 +0800

    small changes

--- admin/account-settings.php
+++ admin/account-settings.php
@@ -75,14 +75,15 @@
                   <a href="../admin/notification.php" data-toggle="tab" class="nav-link has-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg></a>
                 </li>
             </div>
-        <div class="card-body tab-content">
+            
+        <div class="card-body tab-content"><!--Start of services offers-->
         <div class="tab-pane active" id="account">
                 <h6 style="margin-top: 10px;">WEBSITE SETTINGS</h6>
                 <hr>
                 <div class="row mb-3">
-                  <div class="col"><label for="table"><strong>Services</strong></label></div>
+                  <div class="col"><label for="table"><strong>Services</strong></label></div><!--Services-->
                   <div class="col justify-content-end d-flex">
-                    <button class="btn btn-info btn-sm" type="button" style="background: #00ACB2; border: #00ACB2; color:#fff;" data-bs-toggle="modal" data-bs-target="#addservice"><a class="text-white text-decoration-none" href="#"><i class="fa-solid fa-plus me-1"></i> Add Services</a></button>
+                    <button class="btn btn-info btn-sm" type="button" style="background: #00ACB2; border: #00ACB2; color:#fff;" data-bs-toggle="modal" data-bs-target="#addservice"><a class="text-white text-decoration-none" href="#"><i class="fa-solid fa-plus me-1"></i> Add Services</a></button><!--Add services-->
                   </div>
                 </div>
                 <div class="table-responsive">
@@ -170,13 +171,138 @@
           ?>
           </div>
           </div>
-          </div>
+          </div><!--End of services offers-->
 
+          <div class="card-body tab-content">
+        <div class="tab-pane active" id="account">
+                <div class="row mb-3">
+                  <div class="col"><label for="table"><strong>Other Fees</strong></label></div>
+                  <div class="col justify-content-end d-flex">
+                    <button class="btn btn-info btn-sm" type="button" style="background: #00ACB2; border: #00ACB2; color:#fff;" data-bs-toggle="modal" data-bs-target="#addfees"><a class="text-white text-decoration-none" href="#"><i class="fa-solid fa-plus me-1"></i> Add Fees</a></button>
+                  </div>
+                </div>
+                <div class="table-responsive">
+                  <table class="table table-bordered border-dark text-center">
+                  <?php
+                    require_once '../classes/reference.class.php';
+                    $services = new Reference();
+
+                    $service_list = $services->get_services();
+                  ?>
+                  <thead>
+                    <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Price</th>
+                      <th scope="col" >Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php foreach($service_list as $row){ ?>
+                    <tr>
+                      <td><?php echo $row['services'] ?></td>
+                      <td>₱<?php echo number_format($row['price']) ?></td>
+                      <td><a href="#"><i class="fa-solid fa-pen" data-bs-toggle="modal" data-bs-target="#edit-fees<?php echo $row['id'] ?>"></i><i class="fa-solid fa-trash" data-bs-toggle="modal" data-bs-target="#delete-fees<?php echo $row['id'] ?>"></i></a></td>
+                    </tr>
+
+                    <!-- Delete Modal -->
+                    <div class="modal fade" id="delete-fees<?php echo $row['id'] ?>" tabindex="-1" aria-labelledby="delete-feesLabel" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered modal-sm">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="delete-feesLabel">Delete Fees</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body text-center">
+                            Are you sure to delete the selected service?
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <a href="remove-service.php?id=<?php echo $row['id'] ?>" class="btn btn-primary">Yes</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <!-- Edit Modal -->
+                    <div class="modal fade" id="edit-fees<?php echo $row['id'] ?>" tabindex="-1" aria-labelledby="edit-feesLabel" aria-hidden="true">
+                      <div class="modal-dialog modal-sm">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h5 class="modal-title" id="edit-feesLabel">Edit Fees</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                            <form action="account-settings.php" method="POST">
+                              <div class="mb-3">
+                                <label for="services" class="col-form-label"><strong>Name:</strong></label>
+                                <input type="text" class="form-control" id="services" name="services" value="<?php echo $row['services'] ?>">
+                              </div>
+                              <div class="mb-3">
+                              <label for="price" class="col-form-label"><strong>Price:</strong></label>
+                                <input type="number" class="form-control" id="price" name="price" value="<?php echo $row['price'] ?>">
+
+                                <input type="text" class="form-control" id="id" name="id" value="<?php echo $row['id'] ?>" hidden>
+                              </div>
+
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" name="edit">Confirm</button>
+                          </div>
+                        </form>
+                        </div>
+                      </div>
+                    </div>
+
+                  <?php } ?>
+                  </tbody>
+                  </table>
+                </div>
+          <div class="text-center">
+          <?php
+            if(isset($success)){
+                echo '<div><p class="text-success mt-2 mb-2">'.$success.'</p></div>';
+            }     
+          ?>
+          </div>
+          </div>
+          </div> <!--End of services offers-->
+
+<!--Add service-->
 <div class="modal fade" id="addservice" tabindex="-1" aria-labelledby="addserviceLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Add Service</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="account-settings.php" method="POST">
+          <div class="mb-3">
+            <label for="services" class="col-form-label"><strong>Name:</strong></label>
+            <input type="text" class="form-control" id="services" name="services">
+          </div>
+          <div class="mb-3">
+          <label for="price" class="col-form-label"><strong>Price:</strong></label>
+            <input type="number" class="form-control" id="price" name="price">
+          </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary" name="submit">Confirm</button>
+      </div>
+    </form>
+    </div>
+  </div>
+</div>
+
+<!--Add fees-->
+<div class="modal fade" id="addfees" tabindex="-1" aria-labelledby="addfeesLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addfeesLabel">Add Fees</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
